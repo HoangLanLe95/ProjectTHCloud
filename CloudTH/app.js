11c9a0c397d54c4c06b7a68e85836142ef0bddc6
HoangLanLe95 <lvhlan95@gmail.com>
2025-03-22 10:17:10 +0700

    just copy app.js

--- CloudTH/app.js
+++ CloudTH/app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+﻿const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Thông Tin Thành Viên</title>
+        <title>THÔNG TIN THÀNH VIÊN</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
@@ -51,35 +51,45 @@ const server = http.createServer((req, res) => {
     </head>
     <body>
         <div class="container">
-            <h2>Danh Sách Thành Viên</h2>
+            <h2>Danh sách thành viên</h2>
             <table>
                 <thead>
                     <tr>
-                        <th>Họ và Tên</th>
-                        <th>Vai Trò</th>
-                        <th>Email</th>
+                        <th style="text-align:center">MSHV</th>
+                        <th style="text-align:center">Họ và Tên</th>
+                        <th style="text-align:center">Năm sinh</th>
+                        <th style="text-align:center">Email</th>
+                        <th style="text-align:center">SĐT</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Le Vu Hoang Lan</td>
-                        <td>Thanh vien 1</td>
-                        <td>email@example.com</td>
+			<td style="text-align:center">M2524009</td>
+                        <td>Lê Vũ Hoàng Lan</td>
+                        <td style="text-align:center">1995</td>
+                        <td>lanm2524009@gstudent.ctu.edu.vn</td>
+			<td></td>
                     </tr>
                     <tr>
-                        <td>Trần Thu Hien</td>
-                        <td>Thanh vien 2</td>
-                        <td>email@example.com</td>
+                        <td style="text-align:center">M2524007</td>
+                        <td>Trần Thu Hiền</td>
+                        <td style="text-align:center">1996</td>
+                        <td>hienm2524007@gstudent.ctu.edu.vn</td>
+			<td></td>
                     </tr>
                     <tr>
-                        <td>Nguyen Diep Gia Bao</td>
-                        <td>Thanh vien 3</td>
-                        <td>email@example.com</td>
+                        <td style="text-align:center">M2524017</td>
+                        <td>Nguyễn Diệp Gia Bảo</td>
+                        <td style="text-align:center">2000</td>
+                        <td>baom2524017@gstudent.ctu.edu.vn</td>
+			<td></td>
                     </tr>
                     <tr>
-                        <td>Nguyen Van Vu</td>
-                        <td>Thanh vien 4</td>
-                        <td>email@example.com</td>
+                        <td style="text-align:center">M2524028</td>
+                        <td>Nguyễn Văn Vũ</td>
+                        <td style="text-align:center">2000</td>
+                        <td>vum2524028@gstudent.ctu.edu.vn</td>
+			<td></td>
                     </tr>
                 </tbody>
             </table>
